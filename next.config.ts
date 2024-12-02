7e26f04eb4d08dbeb771d4362fb19e96268a3ba4
luvsic3 <youruichao@gmail.com>
2024-12-02 22:08:31 +0800

    test

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/ssg',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
