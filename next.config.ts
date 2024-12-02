439c7bb09aec4793e656e85e418d89682072a8a9
luvsic3 <youruichao@gmail.com>
2024-12-02 22:26:38 +0800

    test

--- next.config.ts
+++ next.config.ts
@@ -3,27 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/_next/data/:id*',
-        headers: [
-          {
-            key: 'Vercel-CDN-Cache-Control',
-            value: 'max-age=3600',
-          },
-          {
-            key: 'CDN-Cache-Control',
-            value: 'max-age=60',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'max-age=10',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
